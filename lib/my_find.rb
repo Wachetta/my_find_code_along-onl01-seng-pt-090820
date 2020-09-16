62b6c55b65b9fb6188f1e3aa3745bd86f58e583b
Zachary Darneille <zakdarn@gmail.com>
2020-09-16 18:10:22 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0 # counter
   while i < collection.length # while counter is less then length of array
-    if yield(collection[i]) #yield and pass element 0 ofcollection
+    if yield (collection[i]) #yield and pass element 0 ofcollection
     return collection[i]     #return the arguement
     i = i + 1
     end
